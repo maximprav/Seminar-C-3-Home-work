365ee95fe0a76c96ee5f90a321081e2441b6ef74
Maxim Prav <mxmlknv@mail.ru>
2023-02-08 18:15:10 +0300

    Задача с кубом

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите любое положительное число:");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+if (number > 0)
+{
+    while (count < number)
+    {
+        Console.Write($"{count*count*count}, ");
+        count++;
+    }
+}
+if (count == number)
+{
+        Console.Write($"{count*count*count}. ");
+}
+else
+{
+    Console.Write($"Задано неверное число.");
+}
